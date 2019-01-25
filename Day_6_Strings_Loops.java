9a5c3cc26278f5190b9e5f45f183206448ea0b0e
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 14:44:04 +1100

    Create Day_6_Strings_Loops.java

---
+++ Day_6_Strings_Loops.java
@@ -0,0 +1,37 @@
+/**
+Author: Atharv Damle
+Full Problem Statement: https://www.hackerrank.com/challenges/30-review-loop/problem
+tl;dr: Separate odd and even characters of entered word and print them separately
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int j = 0; j < n; j++)
+        {
+            String s = sc.nextLine();
+            String e = "";
+            String o = "";
+            for (int i = 0; i < s.length(); i++)
+            {
+                char ch = s.charAt(i);
+                if (i % 2 == 0)
+                {
+                    e = e + ch;
+                }
+                else
+                {
+                    o = o + ch;
+                }
+            }
+            System.out.println(e + ' ' + o);
+        }
+    }
+}
+
