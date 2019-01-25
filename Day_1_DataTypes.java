8df9c30ed8b0251f72f316bc81906e7c037538d3
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 13:52:52 +1100

    Create Day_1_DataTypes.java

---
+++ Day_1_DataTypes.java
@@ -0,0 +1,45 @@
+/**
+// Author: Atharv Damle
+For full problem statement : https://www.hackerrank.com/challenges/30-data-types/problem
+tl;dr: Accept and print variables
+*/
+import java.io.*;
+import java.util.*;
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        double x = 0;
+        int y = 0;
+        String z = "";
+        
+        // Accept an integer
+        y = scan.nextInt();
+        
+        // Accept a real number
+        x = scan.nextDouble();
+        
+        // IMPORTANT: Since the nextInt() and nextDouble() do not accept the '\n' character marking the end of a line, 
+        // the first nextLine() does not accept the next line, but only till the end of current line when the user
+        // pressed the enter key. Thus, two are used.
+        z = scan.nextLine();
+        z = scan.nextLine();
+        
+        
+        // Print the sum of both integer variables on a new line.
+        System.out.println(i + y);
+        // Print the sum of the double variables on a new line.
+        System.out.println(d + x);
+        
+        // The '+' operator when used with strings, joins the two strings.
+        System.out.println(s + z);
+
+        scan.close();
+    }
+}
