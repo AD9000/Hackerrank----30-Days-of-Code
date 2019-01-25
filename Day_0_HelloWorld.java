74cc27bf995286139188dcc2579553483fd1aa81
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 13:43:14 +1100

    Update Day_0_HelloWorld.java

--- Day_0_HelloWorld.java
+++ Day_0_HelloWorld.java
@@ -3,24 +3,24 @@
 Obligatory Hello World! Program. Print Hello World! to Screen.
 One twist: Accept one line and print it to screen too.
 */
-import java.io.*;
+import java.util.*;
 
 public class Day_0_HelloWorld {
 	public static void main(String[] args) {
-    // Create a Scanner object to read input from stdin.
-		Scanner in = new Scanner(System.in); 
-		
-		// Accept a sentence as input
-		String inputString = scan.nextLine(); 
+    	// Create a Scanner object to read input from stdin.
+	Scanner in = new Scanner(System.in); 
 
-		// Close the Scanner object. Not doing this can sometimes lead to errors
-		in.close(); 
-      
-		// Print hello world.
-		System.out.println("Hello, World.");
+	// Accept a sentence as input
+	String inputString = scan.nextLine(); 
+
+	// Close the Scanner object. Not doing this can sometimes lead to errors
+	in.close(); 
+
+	// Print hello world.
+	System.out.println("Hello, World.");
     
-    // print the string that was accepted as input.
-    System.out.println(inputString);
+	// print the string that was accepted as input.
+	System.out.println(inputString);
 	}
 }
 
