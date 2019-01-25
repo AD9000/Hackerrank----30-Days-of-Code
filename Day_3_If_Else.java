3f03c123269369f23064b6e2ab3e08e101194059
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 14:09:39 +1100

    Create Day_3_If_Else.java

---
+++ Day_3_If_Else.java
@@ -0,0 +1,25 @@
+/**
+Author: Atharv Damle
+Full Problem Statement: https://www.hackerrank.com/challenges/30-conditional-statements/problem
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+               
+        if (N % 2 != 0 || N % 2 == 0 && N > 5 && N < 21)
+        {
+            System.out.println("Weird");
+        }
+        else
+        {
+            System.out.println("Not Weird");
+        }
+        scanner.close();
+    }
+}
