5f4a1bd9351e281e26ce8e16b2883952c22723f5
Atharv Damle <32605565+AD9000@users.noreply.github.com>
2019-04-02 21:12:46 +1100

    Create Solution.java

---
+++ Day 28/Solution.java
@@ -0,0 +1,44 @@
+// Author: Atharv Damle
+// To find emails in an input...using regEx (Regular Expressions)
+// Full Question: https://www.hackerrank.com/challenges/30-regex-patterns/problem
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.nextLine();
+
+        LinkedList<String> names = new LinkedList<String>();
+        // Load pattern
+        Pattern p = Pattern.compile(".*@gmail\\.com$");
+
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+
+            // Match pattern
+            Matcher m = p.matcher(emailID);
+            if (m.find())
+            {
+                names.add(firstName);
+            }
+        }
+
+        Collections.sort(names);
+        while(!names.isEmpty())
+        {
+            System.out.println(names.remove());
+        }
+
+        scanner.close();
+    }
+}
